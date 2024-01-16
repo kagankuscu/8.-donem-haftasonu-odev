2b0e75c06508c6501092202894ecba2b33f84b69
kagankuscu <kuscukagan@gmail.com>
2024-01-16 12:04:53 +0200

    Proje Oluşturuldu.

---
+++ DailyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using DailyApp.Db;
+using DailyApp.View;
+using System.Data.SqlClient;
+
+namespace DailyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
